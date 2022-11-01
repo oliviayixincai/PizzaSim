17b0ee20d7f55636f061b9d3e3d9acf8ef8f0408
bobtrolledu <335730800@gapps.yrdsb.ca>
2022-11-01 11:00:41 -0400

    Added Interface Class

--- PizzaSimGame/ProgressBar.java
+++ PizzaSimGame/ProgressBar.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Button here.
+ * Write a description of class ProgressBar here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Button extends Interface
+public class ProgressBar extends Interface
 {
     /**
-     * Act - do whatever the Button wants to do. This method is called whenever
+     * Act - do whatever the ProgressBar wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()

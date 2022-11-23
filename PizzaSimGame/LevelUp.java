b6283b5c82ec4391cd0399ebbd5a30354aa4751b
bobtrolledu <335730800@gapps.yrdsb.ca>
2022-11-22 22:52:32 -0500

    fixed level up robber

--- PizzaSimGame/LevelUp.java
+++ PizzaSimGame/LevelUp.java
@@ -24,9 +24,9 @@ public class LevelUp extends Actor
         if((utils.getResturantMoneyOne() > 100 && utils.getResturantMoneyOne() < 200 && utils.getResturantLevelOne() < 2) || (utils.getResturantMoneyOne() >  200 && utils.getResturantLevelOne() < 3))
         {
             utils.addResturantLevelOne();
-            if(utils.getRobbingMoneyMama() < -10)
+            if(utils.getRobbingMoneyPapa() < -10)
             {
-                utils.setRobbingMoneyMama(5);
+                utils.setRobbingMoneyPapa(5);
             }
             LevelDisplayer level_displayer = (LevelDisplayer)getWorld().getObjectsAt(213, 50, LevelDisplayer.class).get(0);
             level_displayer.setDisplayer(utils.getResturantLevelOne());
@@ -35,9 +35,9 @@ public class LevelUp extends Actor
         if((utils.getResturantMoneyTwo() > 100 && utils.getResturantMoneyTwo() < 200 && utils.getResturantLevelTwo() < 2) || (utils.getResturantMoneyTwo() > 200 && utils.getResturantLevelTwo() < 3))
         {
             utils.addResturantLevelTwo();
-            if(utils.getRobbingMoneyPapa() < -10)
+            if(utils.getRobbingMoneyMama() < -10)
             {
-                utils.setRobbingMoneyPapa(5);
+                utils.setRobbingMoneyMama(5);
             }
             LevelDisplayer level_displayer = (LevelDisplayer)getWorld().getObjectsAt(795, 50, LevelDisplayer.class).get(0);
             level_displayer.setDisplayer(utils.getResturantLevelTwo());
